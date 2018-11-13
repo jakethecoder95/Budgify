46d251dc2fc2d5db25e6f9d44e8f9fbb1f64f5b9
jakethecoder95 <95jacob07@gmail.com>
2018-11-12 22:47:43 -0800

    initial commit

---
+++ Budgify/webpack.config.babel.js
@@ -0,0 +1,64 @@
+﻿const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const nodeExternals = require('webpack-node-externals');
+const fs = require('file-system');
+
+// BACK
+const backConfig = {
+    target: "node",
+    entry:  ["babel-polyfill", "./src/server/test.js"],
+    devServer: {
+        contentBase: './dist/build'
+    },
+    output: {
+        path: path.resolve(__dirname, "./dist/build"),
+        filename: "bundle-back.js"
+    },
+    externals: [nodeExternals(), ".es6", ".es", ".jsx", ".js"],
+    module: {
+        rules: [
+            {
+                type: 'javascript/auto',
+                test: /\.json$/,
+                use: {
+                    loader: 'babel-loader', 
+                    options: {
+                        "presets": ["env"]
+                    }
+                }
+            }
+        ]
+    }
+};
+
+
+// FRONT 
+frontConfig = {
+    entry: ['babel-polyfill', './src/client/index.js'],
+    output: {
+        path: path.resolve(__dirname, './dist/build'),
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: './dist/build'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: '_Layout.cshtml',
+            template: './Views/Shared/_Layout.cshtml'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    }
+}
+
+module.exports = [frontConfig, backConfig];
